af264cf26af9f6285be3a29db0763a5309312860
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-01-13 11:37:42 +0100

    complete

---
+++ migrations/2022-12-16-122550_mining_pool_api/up.sql
@@ -0,0 +1,15 @@
+-- Your SQL goes here
+CREATE TABLE IF NOT EXISTS wallets
+(
+  address         UUID PRIMARY KEY        NOT NULL,
+  club_name       TEXT                    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS miners
+(
+  id              UUID PRIMARY KEY        NOT NULL,
+  address         UUID                    NOT NULL REFERENCES wallets(address),
+  nickname        TEXT                    NOT NULL,
+  hash_rate       INT                     NOT NULL,
+  shares_mined    INT                     NOT NULL
+);
---
+++ src/wallet_controller.rs
@@ -0,0 +1,44 @@
+use {
+  actix_web::HttpResponse,
+  actix_web::web::{Data, Json, Path},
+  uuid::Uuid,
+
+  crate::DBPool,
+  crate::wallet::*,
+  crate::util::{NotFoundMessage, ResponseType},
+};
+
+// List all wallets
+#[get("/wallets")]
+pub async fn list_wallets(pool: Data<DBPool>) -> HttpResponse {
+  let conn = crate::get_connection_to_pool(pool);
+
+  let wallets: Vec<Wallet> = fetch_all_wallets(&conn);
+  ResponseType::Ok(wallets).get_response()
+}
+
+// Get a wallet
+#[get("/wallets/{id}")]
+pub async fn get_wallet(path: Path<(String,)>, pool: Data<DBPool>) -> HttpResponse {
+  let conn = crate::get_connection_to_pool(pool);
+  let wallet: Option<Wallet> = fetch_wallet_by_id(
+    Uuid::parse_str(path.0.0.as_str()).unwrap(), &conn);
+  match wallet {
+    Some(wallet) => ResponseType::Ok(wallet).get_response(),
+    None => ResponseType::NotFound(
+      NotFoundMessage::new("Wallet not found.".to_string())
+    ).get_response(),
+  }
+}
+
+// Create new Wallet
+#[post("/wallets/")]
+pub async fn create_wallet(wallet_request: Json<NewWalletRequest>, pool: Data<DBPool>) -> HttpResponse {
+  let conn = crate::get_connection_to_pool(pool);
+  match create_new_wallet(wallet_request.0, &conn) {
+    Ok(created_wallet) => ResponseType::Created(created_wallet).get_response(),
+    Err(_) => ResponseType::NotFound(
+        NotFoundMessage::new("Error creating wallet.".to_string())
+    ).get_response(),
+  }
+}
